5e7caa25c2f257deb62659e2ded40366b093042d
emines0 <emin.sefa1@gmail.com>
2023-07-03 14:14:42 +0200

    create routes/basic pages

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,13 @@
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components/'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt='jobster logo' className='logo' />
+        <Logo />
       </nav>
       <div className='container page'>
         {/* info */}
@@ -20,54 +21,14 @@ const Landing = () => {
             possimus nihil delectus, necessitatibus, earum vel fugiat molestiae
             illum culpa temporibus!
           </p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
       </div>
     </Wrapper>
   )
 }
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-  }
 
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -2rem;
-  }
-
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-
-  p {
-    color: var(--grey-600);
-  }
-
-  .main-img {
-    display: none;
-  }
-
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr; /* 2 columns layout */
-      column-gap: 3rem;
-    }
-
-    .main-img {
-      display: block;
-    }
-  }
-`
 export default Landing
